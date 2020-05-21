a121b70c74ef038b88ec4aec66b2b339356f0410
Slavin Donchev <zuri-analytics@protonmail.com>
2020-05-21 11:37:48 -0700

    Added cool stuff

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,32 @@
+const htmlmin = require("html-minifier");
+
+
 module.exports = function(eleventyConfig) {
+
+    /**
+     * Include static resources
+     */
     eleventyConfig.addPassthroughCopy('css')
     eleventyConfig.addPassthroughCopy('config')
     eleventyConfig.addPassthroughCopy('images')
     eleventyConfig.addPassthroughCopy('js')
     eleventyConfig.addPassthroughCopy('webfonts')
     eleventyConfig.addPassthroughCopy('CNAME')
+
+    /**
+     * Minify HTML
+     */
+    eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+        if( outputPath.endsWith(".html") ) {
+            return htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+        }
+        return content;
+    });
+
     return {
         passthroughFileCopy: true
     }
